10696ca87843fa391bb20df8fdd2e83b762b06ff
Alex Mikitsik <mikitsik@gmail.com>
2023-10-28 03:20:38 +0300

    Make gem use rack

--- lib/rails_from_scratch.rb
+++ lib/rails_from_scratch.rb
@@ -4,5 +4,12 @@ require_relative "rails_from_scratch/version"
 
 module RailsFromScratch
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Application
+    def call(env)
+      [
+        200, {'Content-Type' => 'text/html'}, ["Hello from Rails from Scratch!"]
+      ]
+    end
+  end
 end
