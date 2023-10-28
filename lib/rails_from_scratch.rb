12c6e002f0510c4efeb04e265d260055bdb01d7b
Alex Mikitsik <mikitsik@gmail.com>
2023-10-28 23:10:27 +0300

    Add controller

--- lib/rails_from_scratch.rb
+++ lib/rails_from_scratch.rb
@@ -2,16 +2,25 @@
 
 require "rails_from_scratch/version"
 require "rails_from_scratch/array"
+require "rails_from_scratch/routing"
 
 module RailsFromScratch
   class Error < StandardError; end
 
   class Application
     def call(env)
-      `echo debug > debug.txt`
-      [
-        200, {'Content-Type' => 'text/html'}, ["Hello from Rails from Scratch!"]
-      ]
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+      [200, {'Content-Type' => 'text/html'}, [text]]
+    end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
     end
   end
 end
